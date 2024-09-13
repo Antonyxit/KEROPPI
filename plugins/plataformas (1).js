aa8ebd9918dee86590a295c5aa524fbe5cbb6be9
Antonyxit <carlosmorga45@gmail.com>
2024-09-13 14:35:12 -0600

    Update plataformas (1).js

--- plugins/plataformas (1).js
+++ plugins/plataformas (1).js
@@ -5,6 +5,30 @@ handler.command = /^(preciolatam)$/i;
 export default handler;
 
 global.preciolatam = `
-PLATAFORMAS DISPONIBLES
-XNXX GOLD
+LOTES y medios
+ ✨NETFLIX  10X$370 5x$185 PEDIDO
+✨VIX 10x$130 5×$66 PEDIDO no desperados,  garantía de 25 días 
+✨LOTE MIXTO 10×$170 ( O DE UNO) crunchy económico 
+CANVA
+1m $15
+2m $22
+3m $30
+1año$40
+2 años$50
+3 años$55
+Inif $60
+   CUENTAS COMPLETAS            1PZ
+🌺 VIX 1M $15
+🌺CRUNCHY $20
+🌺PARAMOUNT $20
+🌺PERFIL PRIVADO  $38
+👾DUOLINGO  FAM $20
+👾TIDAL $15
+👾VIKI 1M $10
+👾YOUTUBE 1M individual  $15
+👾YOUTUBE  invitación  $9
+METODO
+CANVA EXPLICADO EN VIDEO $50
+VPN $40
+Likes,vistas, seguidores para toda app
 `;
