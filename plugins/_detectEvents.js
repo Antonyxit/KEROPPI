f3746b5014c9754681bbda2c836dfccef3751a46
Antonyxit <carlosmorga45@gmail.com>
2024-09-24 13:15:26 -0600

    Update _detectEvents.js

--- plugins/_detectEvents.js
+++ plugins/_detectEvents.js
@@ -20,7 +20,7 @@ export async function before(m, {conn, participants}) {
   if (!m.messageStubType || !m.isGroup) return !0;
   const groupName = (await conn.groupMetadata(m.chat)).subject;
   const groupAdmins = participants.filter((p) => p.admin);
-  const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => null) || 'https://github.com/Antonyxit/KEROPPI/blob/master/src/avatar_contact.jpg';
+  const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => null) || './src/avatar_contact.jpg';
   const img = await (await fetch(pp)).buffer();
   const chat = global.db.data.chats[m.chat];
   const mentionsString = [m.sender, m.messageStubParameters[0], ...groupAdmins.map((v) => v.id)];
